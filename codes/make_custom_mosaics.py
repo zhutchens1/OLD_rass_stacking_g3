f5d4b31af8c352b0956eaf9f1cb22135a3d5cca2
Zack Hutchens <zhutchens1@gmail.com>
2023-06-08 14:03:43 -0400

    added codes-initial commit

---
+++ codes/make_custom_mosaics.py
@@ -0,0 +1,189 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KDTree
+from reproject.mosaicking import reproject_and_coadd, find_optimal_celestial_wcs
+from reproject import reproject_interp, reproject_exact
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.nddata.utils import Cutout2D
+from astropy.coordinates import SkyCoord
+import astropy.units as uu
+import matplotlib.pyplot as plt
+
+def make_custom_mosaics(groupid, groupra, groupdec, count_paths, exp_paths, outsz, outdir, savehandle, method):
+    """
+    Make custom images of galaxy groups at specified RA/Dec using mosaics of raw images.
+
+    Parameters
+    --------------------
+    groupra : iterable
+        RA values where images should be extracted, decimal degrees.
+    groupdec : iterable
+        Dec values where images should be extracted, decimal degrees.
+    countpaths : iterable
+        Sequence of array_like objects containing the filenames of the closest
+        count maps to each group, e.g. as returned from get_neighbor_images.
+    exp_paths : iterable
+        Sequence of array_like objects containing the filenames of the closest
+        exposure maps to each group, e.g. as returned from get_neighbor_images.
+        These should respond one-to-one with the counts maps in `countpaths`.
+    outsz : scalar
+        Output size of extracted, custom image. The final image will be square,
+        e.g. outsz=500 will return a 500 x 500 image.
+    outdir : str
+        Directory where output images will be written.
+    savehandle : array-like, str elements
+        Identifier of the image in final filename, e.g. group ID and or central galaxy name.
+        Example: if savehandle='10830', then the file will be saved as RASS-Cnt_Broad_grp10830.fits
+        in outdir. Length matches groupra, groupdec
+    method : callable
+        Method of resampling pixels, must be either reproject.reproject_interp
+        or reproject.reproject_exact.
+
+    Returns
+    -----------------------
+    None. Images are mosaicked, extracted, and written to disk. 
+    """
+    assert callable(method), "Parameter `method` must reproject.reproject_interp or reproject.reproject_exact."
+    groupid=np.array(groupid)
+    groupra=np.array(groupra)
+    groupdec=np.array(groupdec)
+    coords=SkyCoord(ra=groupra*uu.degree, dec=groupdec*uu.degree)
+    count_paths=np.array(count_paths,dtype=object)
+    exp_paths=np.array(exp_paths,dtype=object)
+    
+    for ii,gg in enumerate(groupid):
+        cname=count_paths[ii]
+        ename=exp_paths[ii]
+        chdus = [fits.open(cm)[0] for cm in cname]
+        ehdus = [fits.open(em)[0] for em in ename]
+        wcs_out,shape_out = find_optimal_celestial_wcs(chdus)
+        cmosaic, cfp = reproject_and_coadd(chdus,output_projection=wcs_out,shape_out=shape_out,reproject_function=method)
+        emosaic, efp = reproject_and_coadd(ehdus,output_projection=wcs_out,shape_out=shape_out,reproject_function=method)
+        extract_write_from_mosaic(cmosaic,coords[ii],wcs_out,outsz,outdir+"RASS-Cnt_Broad_grp"+savehandle[ii]+".fits")
+        extract_write_from_mosaic(emosaic,coords[ii],wcs_out,outsz,outdir+"RASS-Exp_Broad_grp"+savehandle[ii]+".fits")
+
+def mosaic_single_group(groupid, groupra, groupdec, count_paths, exp_paths, outsz, outdir, savehandle, method):
+    cname=count_paths
+    ename=exp_paths
+    coords=SkyCoord(ra=groupra*uu.degree,dec=groupdec*uu.degree)
+    chdus = [fits.open(cm)[0] for cm in cname]
+    ehdus = [fits.open(em)[0] for em in ename]
+    wcs_out,shape_out = find_optimal_celestial_wcs(chdus)
+    cmosaic, cfp = reproject_and_coadd(chdus,output_projection=wcs_out,shape_out=shape_out,reproject_function=method)
+    emosaic, efp = reproject_and_coadd(ehdus,output_projection=wcs_out,shape_out=shape_out,reproject_function=method)
+    extract_write_from_mosaic(cmosaic,coords,wcs_out,outsz,outdir+"RASS-Cnt_Broad_grp"+savehandle+".fits")
+    extract_write_from_mosaic(emosaic,coords,wcs_out,outsz,outdir+"RASS-Exp_Broad_grp"+savehandle+".fits")
+
+
+def extract_write_from_mosaic(mosaic,position,wcs,outsz,savepath):
+    """
+    Extract a 2D cutout from a mosaic image.
+
+    Parameters
+    ----------------------------
+    mosaic : np.array
+        Numpy array representing the mosaic.
+    position : astropy.coordinates.SkyCoord instance
+        On-sky position at which to perform the extraction.
+    wcs : astropy.WCS instance
+        World coordinate system of the mosaic.
+    outsz : int
+        Size of image to be extracted and written.
+        (e.g., 500 x 500 if outsz=500).
+    savepath : str
+        Location where cutout image should be saved.
+    """
+    image = Cutout2D(mosaic,position=position,wcs=wcs,size=outsz)
+    hdu = fits.PrimaryHDU(image.data, header=image.wcs.to_header())
+    hdulist=fits.HDUList([hdu])
+    hdulist.writeto(savepath,overwrite=True)
+
+def get_neighbor_images(groupra, groupdec, imagera, imagedec, imagename, kk=9):
+    """
+    Given a set of galaxies or galaxy groups, and a separate set of 
+    images, find the k neighboring images surrounding each group (incl.
+    the image that contains the group of interest.) This algoritm implements
+    sklearn.neighbors.KDTree to find images nearby to groups.
+
+    Parameters
+    ------------------------
+    groupra : iterable
+        RA of group centers, size N, where N is the number
+        of unique groups in the catalog. Decimal degrees.
+    groupdec : iterable
+        Declination of group centers, size N. Decimal degrees.
+    imagera : iterable
+        RA of image centers, size p. Decimal degrees.
+    imagedec : iterable
+        Declination of image centers, size p. Decimal degrees.
+    imagename : iterable
+        Array of identificiation values for each image. Can be strings,
+        int, etc. Size p.
+    kk : int, default 9
+        Number of images to return for each group. This argument is
+        passed as `k` to sklearn.neighbors.KDTree.
+        
+    Returns
+    -------------------------
+    neighbors : np.array of shape (N,kk)
+        Matrix containing the kk-nearest images, denoted using values
+        rom `imagename`, for each observation in the group* dataset.
+    """
+    groupra=np.array(groupra)
+    groupdec=np.array(groupdec)
+    imagera=np.array(imagera)
+    imagedec=np.array(imagedec)
+    dtr=np.pi/180.
+    imageX = np.sin(np.pi/2.-imagedec*dtr)*np.cos(imagera*dtr) 
+    imageY = np.sin(np.pi/2.-imagedec*dtr)*np.sin(imagera*dtr)
+    imageZ = np.cos(np.pi/2.-imagedec*dtr)
+    groupX = np.sin(np.pi/2.-groupdec*dtr)*np.cos(groupra*dtr) 
+    groupY = np.sin(np.pi/2.-groupdec*dtr)*np.sin(groupra*dtr)
+    groupZ = np.cos(np.pi/2.-groupdec*dtr)
+    imagename=np.array(imagename,dtype=object)
+    XX = np.array([imageX,imageY,imageZ])
+    tree = KDTree(np.array(XX.T))
+    groupdata = np.array([groupX,groupY,groupZ])
+    idx = tree.query(groupdata.T, kk, return_distance=False)
+    neighbors = imagename[idx]
+    return neighbors
+
+
+def mosaicfunc(grpid, grpra, grpde, cmaps, emaps):
+     mosaic_single_group(grpid,grpra,grpde,cmaps,emaps,512,'./testimages/',\
+            grpid.astype(str), method=reproject_exact)
+
+if __name__=='__main__':
+    eco = pd.read_csv("../g3groups/ECOdata_G3catalog_luminosity.csv")
+    eco = eco[eco.g3fc_l==1]
+ 
+    rasstable = pd.read_csv("/srv/one/zhutchen/rass_stacking_g3/RASS_public_contents_lookup.csv")
+    econame=np.array(eco.name,dtype=object)
+    names=get_neighbor_images(eco.g3grpradeg_l, eco.g3grpdedeg_l, rasstable.ra, rasstable.dec, rasstable.image, 5)
+
+    exposuremaps=np.zeros_like(names,dtype='object')
+    countmaps=np.zeros_like(names,dtype='object')
+    for ii,subarr in enumerate(names):
+        for jj,name in enumerate(subarr):
+            obs=name.split('.')[0]
+            countmaps[ii][jj]='../rass/'+obs+'/'+obs+'_im1.fits'
+            exposuremaps[ii][jj]='../rass/'+obs+'/'+obs+'_mex.fits'
+
+    grpid = np.array(eco.g3grp_l)
+    grpra = np.array(eco.g3grpradeg_l)
+    grpde = np.array(eco.g3grpdedeg_l)
+    import time
+
+    use_mp=1
+    tt=time.time()
+    if use_mp:
+        import multiprocessing
+        sel = np.where(grpid==14.)
+        args=[grpid[sel],grpra[sel],grpde[sel],countmaps[sel],exposuremaps[sel]]
+        args = [tuple(x) for x in zip(*args)]
+        print(args)
+        #mosaicfunc(*args[0])
+        pool=multiprocessing.Pool()
+        pool.starmap(mosaicfunc,args)
+    print(time.time()-tt)
---
+++ codes/rass_processing.py
@@ -0,0 +1,151 @@
+"""
+RASS processing file
+contains code for point source masking,
+image rescaling, S/N calc etc.
+"""
+from scipy import ndimage
+import numpy as np
+import pandas as pd
+from astropy.io import fits
+from astropy.stats import sigma_clipped_stats
+from astropy.nddata import Cutout2D
+from astropy.wcs import WCS
+import astropy.units as uu
+from astropy.coordinates import SkyCoord
+from photutils import DAOStarFinder, CircularAperture
+from photutils.segmentation import make_source_mask
+from scipy.ndimage import gaussian_filter
+import os
+import sys
+import pickle
+import gc
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+#import multiprocessing 
+from pathos.multiprocessing import ProcessingPool 
+
+def mask_catl_sources(infiledirec, outfiledirec, source_ra, source_dec, apsize, use_mp=0):
+    """
+    Mask point sources in RASS count maps identified in external
+    point source catalogs.
+
+    in the ROSAT 2RXS point source catalog. 
+
+    Parameters
+    ------------------------
+    infiledirec : str
+        Read-in directory containing ROSAT FITS count maps.
+    outfiledirec : str
+        Write-out directory where masked FITS count maps will be written.
+    source_ra : array_like
+        Right ascension (J2000) of point sources in decimal degrees.
+    source_dec : array_like
+        Declination (J2000) of point sources in decimal degrees.
+    apsize : float
+        Size of apertures used in masks in pixel units (e.g., 5 pixels).
+    use_mp : int
+        Number of cores to use for multiprocessing. Must be <=os.cpu_count().
+        If 0 (default), multiprocessing is not implemented.
+
+    Returns
+    ------------------------
+    None. Masked images are saved to disk at `outfiledirec`.
+    """
+    countmapfiles = os.listdir(infiledirec)
+    source_ra = np.array(source_ra)*uu.degree
+    source_dec = np.array(source_dec)*uu.degree
+    sources = SkyCoord(source_ra, source_dec)
+    def mp_worker_func(fname):
+        return mask_catl_sources_worker(fname,infiledirec,outfiledirec,sources,apsize)
+    use_mp=int(use_mp)
+    if use_mp==0:
+        for ff in countmapfiles:
+            mp_worker_func(ff)
+    else:
+        assert use_mp<=os.cpu_count(),"Requested more cores than available on machine."
+        pool = ProcessingPool(use_mp)
+        pool.map(mp_worker_func, countmapfiles)
+
+def mask_catl_sources_worker(imgpath,infiledirec,outfiledirec, sources, apsize):
+    if 'Cnt' in imgpath:
+        print('Removing point sources from '+imgpath)
+        hdulist = fits.open(infiledirec+imgpath,memmap=False)
+        image = hdulist[0].data
+        wcs = WCS(hdulist[0].header)
+        sources_in_image = sources[np.where(wcs.footprint_contains(sources))]
+        pixellocs = wcs.world_to_pixel(sources_in_image)
+        pixellocs = np.array([pixellocs[0],pixellocs[1]]).T
+        apertures = CircularAperture(pixellocs,r=apsize)
+        indiv_mask=apertures.to_mask(method='center')
+        final_mask=np.sum(np.array([msk.to_image(image.shape) for msk in indiv_mask]),axis=0)
+        final_mask = 1-final_mask # convert 1s to 0s, vice versa
+        newimage = image.copy()*final_mask
+        hdulist[0].data = newimage
+        hdulist.writeto(outfiledirec+imgpath,overwrite=True)
+        hdulist.close()
+        del image
+        del newimage
+        del hdulist
+
+
+####################################################
+####################################################
+####################################################
+def scale_subtract_images(imagefiledir, outfiledir, czfiledict, crop=False, imwidth=300, res=45, H0=70., czmin=2530, czmax=7470, progressConf=False):
+    """
+    Scale images to a common redshift. Images must be square.
+    
+    Parameters
+    -------------
+    imgfiledir : str
+        Path to directory containing input images for scaling.
+        Each FITS file in this directory must be named consistently
+        with the rest of this program (e.g. RASS-Int_Broad_grp13_ECO03822.fits).
+    outfiledir : str
+        Path where scaled images should be written.
+    crop : bool, default False
+        If True, all images are cropped to size of the smallest scaled images.
+        This may preferable if the zero-padding on image borders will produce
+        artefacts in other analysis --- cropping maintains uniform noise
+        between each output image, but all images are resized according to the
+        largest and smallest cz values.
+    imwidth : int
+        Width of image, assumed to be square, in pixels; default 300.
+    res : float
+        Pixel resolution in arcseconds, default 45.
+    H0 : float
+        Hubble constant in km/s/(Mpc), default 70.
+    czmin, czmax : float
+        Min and max cz values for all groups, km/s.
+    progressConf : bool, default False
+        If True, the loop prints out a progress statement when each image is finished.
+
+    Returns
+    -------------
+    Scaled/subtracted images are written to the specified path.
+    """
+    imagenames = np.array(os.listdir(imagefiledir))
+    imageIDs = np.array([float(imgnm.split('_')[2][3:-5]) for imgnm in imagenames])
+    if crop: # work out what area to retain
+        D1 = (imwidth*res/206265)*(czmin/H0)
+        Npx = int((D1*H0)/czmax * (206265/res))
+        Nbound = (imwidth-Npx)//2 # number of pixels spanning border region
+    for k in range(0,len(imagenames)):
+        hdulist = fits.open(imagefiledir+imagenames[k], memap=False)
+        img = hdulist[0].data
+        czsf = self.grpcz[self.grpid==imageIDs[k]]/czmax
+        if len(czsf)==0:
+            continue
+        img = ndimage.geometric_transform(img, scale_image, cval=0, extra_keywords={'scale':czsf, 'imwidth':imwidth})
+        if crop: # work out which pixels to retain
+            hdulist[0].data = img
+            wcs = WCS(hdulist[0].header)
+            sel=(self.grpid==imageIDs[k])
+                croppedim = Cutout2D(img, SkyCoord(self.grpra[sel]*uu.degree,self.grpdec[sel]*uu.degree,frame='fk5'), int(imwidth-2*Nbound), wcs)
+                hdu = fits.PrimaryHDU(croppedim.data, header=croppedim.wcs.to_header())
+                hdulist=fits.HDUList([hdu])
+            else:
+                hdulist[0].data = img
+            hdulist.writeto(outfiledir+imagenames[k], overwrite=True)
+            hdulist.close()
+            if progressConf: print("Image {} complete.".format(k))
